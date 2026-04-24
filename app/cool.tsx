1b5d46b9028a80e549b08a5210bbbb5f5f122f5a
Chuks <chuksezeaku@gmail.com>
2026-04-24 23:01:46 +0100

    New project created

---
+++ app/cool.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cool = () => {
+  return (
+    <div>cool</div>
+  )
+}
+
+export default Cool
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>headeH</div>
+  )
+}
+
+export default Header
